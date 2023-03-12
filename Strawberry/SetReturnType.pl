4db5b5ce6486a63dd54ecaa6d1bd838b5e239172
patrick4k <pmkennedy4@gmail.com>
2023-03-11 23:14:22 -0500

    Small changes + Remove cmake dir

--- Strawberry/SetReturnType.pl
+++ Strawberry/SetReturnType.pl
@@ -1,17 +1,20 @@
 use strict;
 use warnings;
 
+# This script opens generated ANTLR visitor files and modifies them to include a custom return type and default methods
+
 sub main {
 
     my $filename = $ARGV[0];
     my $replacement = $ARGV[1];
 
+    # open to read file lines, open to write
     open IN, '<', $filename or die $!;
-
     my @lines = ();
     push(@lines, $_) while (<IN>);
+    close IN;
+    open OUT, '>', $filename or die $!;
 
-    my $OUT = '';
     my $haveAddedH = 0;
     $haveAddedH = 1 if join('',@lines) =~ /#include "..\/Cpp\/$replacement.h"\n#include "..\/Cpp\/StrawberryTree.h"/;
 
@@ -25,12 +28,9 @@ sub main {
         s/std::any/Value/;
         s/return visitChildren\(ctx\)/return StrawberryTree::visitChildrenValue\(ctx\)/;
 
-        $OUT .= $_;
+        print OUT;
     }
 
-    close IN;
-    open OUT, '>', $filename or die $!;
-    print OUT $OUT;
     close OUT;
 
 }
